e5a855d8d9aa569151f25abd45426df60d8e3f15
queenzy black <queenzyblack176@gmail.com>
2021-03-13 22:25:06 +0300

    add shade A page

---
+++ fun.js
@@ -0,0 +1,4 @@
+var greeting=function(){
+    document.getElementById("queen").innerHTML="<p class='joy'>Your production in Shed A 510 litres per day <br> Your production in Shed B 308 litres per day <br>Your production in Shed C 486 litres per day <br> Your production in Shed D 572 litres per day <br><br> The total production is 1876 litres per day</p>"
+    
+}
