fdf235a32018812f44325e57e260d33131beb055
queenzy black <queenzyblack176@gmail.com>
2021-03-13 23:10:29 +0300

    add the week

--- fun.js
+++ fun.js
@@ -1,4 +1,31 @@
-var greeting=function(){
-    document.getElementById("queen").innerHTML="<p class='joy'>Your production in Shed A 510 litres per day <br> Your production in Shed B 308 litres per day <br>Your production in Shed C 486 litres per day <br> Your production in Shed D 572 litres per day <br><br> The total production is 1876 litres per day</p>"
-    
+var totalProduction = function(){
+    //create an array variable to store the sales per shade in one day
+    var shades = [];
+    //push the data that the user will give into the array.
+    shades.push(document.getElementById("shade-a").value);
+    shades.push(document.getElementById("shade-b").value);
+    shades.push(document.getElementById("shade-c").value);
+    shades.push(document.getElementById("shade-d").value);
+    //the values of the array are hereby transfered into variables a,b,c,d
+    var [a,b,c,d] = shades;
+    var displayShadesSales = document.getElementById("display_shades_sales");
+    if(a.length === 0 || b.length === 0 || c.length === 0 || d.length === 0){
+        displayShadesSales.innerHTML = "<p>Please fill in the values to calculate sales</p>"
+    }else{
+    //the user input is converted from string variables, to number variables
+        a = parseFloat(a);
+        b = parseFloat(b);
+        c = parseFloat(c);
+        d = parseFloat(d);
+        totalProductionCalculation(a,b,c,d)
+    }
+}
+var totalProductionCalculation = function(a,b,c,d){
+    var total = a + b + c + d;
+    document.getElementById("results").innerHTML = " <p>Your production in Shed A " + 
+        a + " litres per day <br> Your production in Shed B " + 
+        b + " litres per day <br> Your production in Shed C " + 
+        c + " litres per day <br> Your production in Shed D " +
+        d + " litres per day <br><br>"
+    document.getElementById("total").innerHTML = "The total production is " +total + " litres per day"
 }
