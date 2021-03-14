b643ce569cc12bdf0ee7904503934472eff3c2fe
queenzy black <queenzyblack176@gmail.com>
2021-03-14 15:47:58 +0300

    made other changes

--- fun.js
+++ fun.js
@@ -29,3 +29,19 @@ var totalProductionCalculation = function(a,b,c,d){
         d + " litres per day <br><br>"
     document.getElementById("total").innerHTML = "The total production is " +total + " litres per day"
 }
+
+var finance=function(){
+    var selling_price=45
+    var noOfdays=document.getElementById("days").value
+    time= parseFloat(noOfdays);
+incomeOverTime(selling_price, time)
+}
+
+var incomeOverTime=function(selling_price, time){
+    if(time===7){
+        document.getElementById("time-display").innerHTML="<p>Your weekly income will be " + (1876*selling_price*time) + "</p>"
+    }
+        else if(time===365){
+            document.getElementById("time-display").innerHTML="<p>Your yearly income will be " + (1876*selling_price*time) + "</p>"
+        }
+}
